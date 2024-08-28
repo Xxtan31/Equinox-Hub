8fad24a51e5799b76d71f8949cc7017b083c1b19
Xxtan31 <102824999+Xxtan31@users.noreply.github.com>
2024-08-28 22:09:07 +0300

    Create directions.lua

---
+++ Aimbots/directions.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 17625359962 or game.PlaceId == 18126510175 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Xxtan31/Equinox-Hub/main/Aimbots/(Rivals)", true))()
+   else
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/Xxtan31/Equinox-Hub/main/Aimbots/aimbotNew.lua", true))()
+end
